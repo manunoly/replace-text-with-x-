1e41f29184d0cf30b399ebd3b03d6308c1179604
manunoly <manunoly@gmail.com>
2019-08-05 14:56:58 -0500

    final version complete code

--- index.ts
+++ index.ts
@@ -12,14 +12,18 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
     } else
       dataR = data;
 
-    console.log(dataR.slice(dataR.length-2));
+    let finalD = dataR.slice(dataR.length-2);
+    if (dataT == 'email')
+      finalD = finalD + data.split('@')[1];
+
     let i = 0;
-    let newText;
+    let newText = '';
     while(i < dataR.length-2){
       newText = newText + 'x'
       i = i + 1;
     }
-    console.log(newText);
+
+    console.log(newText + finalD);
   } 
-  
-  hideData('manuel@gmail.com', 'email');
+
+  hideData('manuel@gmail.com', 'email'); 
